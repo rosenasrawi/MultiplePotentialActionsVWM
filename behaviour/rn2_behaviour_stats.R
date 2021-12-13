35174140727f8a36e49f3a9d0be85b386c4ab0f3
Rose Nasrawi <rosenasrawi@Roses-MacBook-Pro.local>
2021-12-13 14:05:16 +0100

    small description change

--- behaviour/rn2_behaviour_stats.R
+++ behaviour/rn2_behaviour_stats.R
@@ -1,4 +1,4 @@
-# Make sure you have summary.rt.subj.load & [...]err.subj.load in your environment (obtained from running "rn2_epoch_conditions.R")
+# Make sure you have summary.rt.subj.load & [...]err.subj.load in your environment (obtained from running "rn2_summary_conditions.R")
 
 anov_RT <- aov(RT1 ~ BlockType, data = summary.rt.subj.load)
 summary(anov_RT)
