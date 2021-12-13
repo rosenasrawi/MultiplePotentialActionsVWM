cfc9e2e49fb51b9e2a620c5ee733080d38375881
Rose Nasrawi <rosenasrawi@Roses-MacBook-Pro.local>
2021-12-13 17:55:48 +0100

    remove ^M

--- EEG/pre-processing/rn2_encoding_ICA_components.m
+++ EEG/pre-processing/rn2_encoding_ICA_components.m
@@ -11,12 +11,13 @@ for this_subject = subjects
     [param, eegfiles, logfiles] = rn2_gen_param(this_subject);
     
     %% Load data
+    
     load(param.path_eegfile); 
 
     data = d;
 
     %% run fast ICA and check eog component detectability (+ topography check)
-
+    
     cfg = [];
     cfg.keeptrials = 'yes';
     cfg.channel = {'EEG'};
@@ -24,20 +25,22 @@ for this_subject = subjects
     d_eeg = ft_timelockanalysis(cfg, data);
 
     %% ica
+    
     cfg = [];
     cfg.method = 'fastica';
     ica = ft_componentanalysis(cfg, d_eeg);
 
-    %% look at components
+    %% Look at components
+    
     cfg           = [];
     cfg.component = 1:61;      
     cfg.layout    = 'easycapM1.lay';
     cfg.comment   = 'no';
     figure; ft_topoplotIC(cfg, ica)
     colormap('jet')
-
-    %% correlate ica timecourses with measured eog
-
+    
+    %% Correlate ICA timecourses with measured eog
+    
     cfg = [];
     cfg.keeptrials = 'yes';
     d_ica = ft_timelockanalysis(cfg, ica);
@@ -46,6 +49,7 @@ for this_subject = subjects
     d_eog = ft_timelockanalysis(cfg, data);
 
     %% Plot (visual inspection of components)
+    
     y = [];
 
     x = d_eog.trial(:,1,:); % eog
@@ -59,14 +63,13 @@ for this_subject = subjects
     xlabel('comp #');
 
     %% Find the max abs cor
-
     max_abs_cor = find(abs(correlations) == max(abs(correlations)));
     max_abs_cor % print which one it is
     
     ica2rem = input('bad components are: ');
 
     %% Save
-        
+    
     save([param.path, '/ICA components/' 'ICA_encoding_components', '_' param.sID], 'ica2rem','ica');
     
 end
